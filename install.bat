8b3792cbbdc07556cf25023b8b5607ccc9c739f4
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-05-27 17:22:23 +0200

    added former config files

---
+++ install.bat
@@ -0,0 +1,41 @@
+@echo off
+
+REM @REM Alacritty configuration
+REM set "sourceFile=%USERPROFILE%\dotfiles\main\alacritty\alacritty.yml"
+REM set "destinationDir=%APPDATA%\alacritty"
+REM set "destinationFile=%APPDATA%\alacritty\alacritty.yml"
+REM if exist "%destinationFile%" (
+REM     del "%destinationFile%"
+REM )
+REM mkdir "%destinationDir%"
+REM mklink "%destinationFile%" "%sourceFile%"
+
+@REM Powershell Profile configuration
+set "sourceFile=%USERPROFILE%\Documents\windows-configuration\powershell\default\powershell\Microsoft.PowerShell_profile.ps1"
+set "destinationDir=%USERPROFILE%\Documents\Powershell"
+set "destinationFile=%USERPROFILE%\Documents\Powershell\Microsoft.PowerShell_profile.ps1"
+if exist "%destinationFile%" (
+    del "%destinationFile%"
+)
+mkdir "%destinationDir%"
+mklink "%destinationFile%" "%sourceFile%"
+
+@REM ideavimrc configuration
+set "sourceFile=%USERPROFILE%\windows-configuration\ideavimrc\default\ideavimrc\.ideavimrc"  
+set "destinationDir=%USERPROFILE%"
+set "destinationFile=%USERPROFILE%\.ideavimrc
+if exist "%destinationFile%" (
+    del "%destinationFile%"
+)
+mklink "%destinationFile%" "%sourceFile%"
+
+@REM vimrc configuration
+set "sourceFile=%USERPROFILE%\windows-configuration\vimrc\default\vimrc\.vimrc"  
+set "destinationDir=%USERPROFILE%"
+set "destinationFile=%USERPROFILE%\.vimrc
+if exist "%destinationFile%" (
+    del "%destinationFile%"
+)
+mklink "%destinationFile%" "%sourceFile%"
+
+@REM autohotkey configuration
