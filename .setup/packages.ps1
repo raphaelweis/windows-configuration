86621c2ea3d778075d487d1fb8e03f3f964fe2c7
Raphaël Weis <raphael.weis@gmail.com>
2023-05-29 09:23:30 +0200

    added firefox dev package

--- .setup/packages.ps1
+++ .setup/packages.ps1
@@ -9,3 +9,4 @@ winget install -e --id Microsoft.PowerToys
 winget install -e --id ApacheFriends.Xampp.8.2
 winget install -e --id CoreyButler.NVMforWindows
 winget install -e --id JetBrains.Toolbox
+winget install -e --id Mozilla.Firefox.DeveloperEdition
